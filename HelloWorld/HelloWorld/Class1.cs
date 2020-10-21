9b87b711f38aae581792d638865f72cd116bf26a
jack5556 <jackson@mobotory.net>
2020-10-21 15:59:01 -0500

    first commit

---
+++ HelloWorld/HelloWorld/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("hello world", "asdf", "0.0")]
+    public class Class1 : RustPlugin
+    {
+
+        void Init()
+        {
+            Puts("Init!");
+        }
+
+        void OnPlayerConnected(Network.Message packet)
+        {
+            //Put kit
+        }
+
+    }
+}
